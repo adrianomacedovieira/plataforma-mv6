bc2555a4190bdf176bb99c5f277a4ef1fee74308
adrianomacedovieira <adrianomacedovieira@gmail.com>
2025-05-22 12:47:26 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,4 @@ path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
     sys.path.insert(0, path)
 
-from src.main import app as application
+from main import app as application
